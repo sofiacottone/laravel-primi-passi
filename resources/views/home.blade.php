12e07367e48a4e5ca245769bedfa75d9cd96b2dd
sofiacottone <cottonesofia@outlook.it>
2024-05-17 15:45:00 +0200

    feat: print data

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,7 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Hello Laravel!</h1>
+    <img src="{{ $logo }}" alt="{{ $name }}"/>
+    <h1>{{ $greeting }} {{ $name }}!</h1>
 </body>
 </html>
